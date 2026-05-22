1da202f7f758ba521267c809f2a49280aec91b64
fredyyfajarr <fredyfajar46@gmail.com>
2026-05-22 18:48:06 +0700

    Fix Network Error by forcing API fallback to IPv4 127.0.0.1

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '8000',
         pathname: '/storage/**',
       },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/storage/**',
+      },
     ],
   },
 };
